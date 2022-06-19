484b3632f1c2657dd4328b3fc6417f89a03b789c
Evan Lynch <evan.f.lynch@gmail.com>
2022-06-19 12:29:43 -0400

    Fix it up

--- README.md
+++ README.md
@@ -2,3 +2,20 @@
 
 ## Features / Bugs ##
 
+
+## Explanation ##
+
+With the root C selected:
+C       C : root
+D♭      ♭ : minor second 
+D       2 : major second
+E♭      m : minor third
+E       M : major third
+F       4 : fourth
+F#      T : tritone
+G       5 : fifth
+G#/A♭   + : augmented fifth / minor sixth
+B♭♭/A   ○ : diminished seventh / major sixth
+B♭      7 : minor seventh
+B       △ : major seventh
+
--- neck/src/app.js
+++ neck/src/app.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import update from 'immutability-helper';
 
-const SymbolList = ["●", "♭", "2", "m", "M", "4", "T", "5", "+", "○", "7", "△"];
+const SymbolList = ["", "♭", "2", "m", "M", "4", "T", "5", "+", "○", "7", "△"];
 
 const RootNotes = ["C", "D♭", "D", "E♭", "E", "F", "G♭", "G", "A♭", "A", "B♭", "B"];
 
@@ -11,41 +12,52 @@ const GetRoot = (letter) => {
 const NUT = 7;
 const PADDING = 5;
 const ROOTS = 40;
-const SELECTED_BACKGROUND_COLOR = "#616f39";
-const SELECTED_TEXT_COLOR = "#ffd98e";
-const UNSELECTED_BACKGROUND_COLOR = "#ffd98e";
-const UNSELECTED_TEXT_COLOR = "#616f39";
+const CHORDS = 42;
+const TUNINGS = 40;
+
+function arrayEquals(a, b) {
+    return Array.isArray(a) &&
+        Array.isArray(b) &&
+        a.length === b.length &&
+        a.every((val, index) => val === b[index]);
+}
 
 
+const maybeClass = (flag, name) => (flag ? name : "");
 
-const Symbol = ({sym, emphasized, select, size}) => {
+const Symbol = ({sym, rootNote, emphasized, select, size}) => {
     const margin = 2;
     return (
-        <div style={{
-            color: emphasized ? SELECTED_TEXT_COLOR : UNSELECTED_TEXT_COLOR,
-            backgroundColor: emphasized ? SELECTED_BACKGROUND_COLOR : UNSELECTED_BACKGROUND_COLOR,
-            height: size - 2 * margin,
-            width: size - 2 * margin,
-            fontSize: size * 0.6,
-            borderRadius: (size - 2 * margin) * 0.5,
-            boxShadow: "rgba(0, 0, 0, 0.4) 1px 2px",
-            margin: margin,
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center"
-        }} onClick={(e)=>{
-            select(sym);
-        }} >{SymbolList[sym]}</div>
+        <div
+            className={[
+                "selectable",
+                "symbol",
+                maybeClass(emphasized, "emphasized"),
+                maybeClass(sym === 0, "root")
+            ].join(" ")}
+            style={{
+                height: size - 2 * margin,
+                width: size - 2 * margin,
+                fontSize: size * 0.6,
+                borderRadius: (size - 2 * margin) * 0.5,
+                boxShadow: "rgba(0, 0, 0, 0.4) 1px 2px",
+                margin: margin,
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center"
+            }} onClick={(e)=>{
+                select(sym);
+            }} >{SymbolList[sym] || rootNote}</div>
     );
 };
 
 const Nut = ({size, horizontal}) => {
     return (
         <span style={{
-            height: horizontal ? size : 2,
-            width: horizontal ? 2 : size,
-            backgroundColor: "black",
-            margin: 2.5,
+            height: horizontal ? size : 3,
+            width: horizontal ? 3 : size,
+            backgroundColor: "gray",
+            margin: 1.5,
         }}></span>
     );
 }
@@ -58,7 +70,7 @@ const String = ({size, horizontal, zero, rootNote, symbols, length, selectSymbol
         }
         const symbol = (i + zero - GetRoot(rootNote) + SymbolList.length) % SymbolList.length;
         let emphasized = symbols.indexOf(symbol) > -1;
-        symbs.push(<Symbol size={size} emphasized={emphasized} key={i} sym={symbol} select={selectSymbol}/>);
+        symbs.push(<Symbol rootNote={rootNote} size={size} emphasized={emphasized} key={i} sym={symbol} select={selectSymbol}/>);
     }
     return (
         <div style={{
@@ -90,9 +102,14 @@ const Neck = ({zeros, size, horizontal, rootNote, symbols, length, selectSymbol}
 
 const RootSelect = ({rootNote, onClick, selected}) => {
     return (
-        <button style={{
-            color: selected ? SELECTED_TEXT_COLOR: UNSELECTED_TEXT_COLOR,
-            backgroundColor: selected ? SELECTED_BACKGROUND_COLOR : UNSELECTED_BACKGROUND_COLOR,
+        <button 
+            className={[
+                "rootSelector",
+                "selectable",
+                maybeClass(selected, "emphasized"),
+            ].join(" ")}
+            
+            style={{
             width: ROOTS,
             display: "flex",
             fontSize: 14,
@@ -104,37 +121,47 @@ const RootSelect = ({rootNote, onClick, selected}) => {
     );
 }
 
-const ChordSelect = ({name, selectSymbols, symbols}) => {
+const ChordSelect = ({horizontal, selectedSymbols, rootNote, name, selectSymbols, symbols}) => {
     return (
-        <button style={{
-            backgroundColor: SELECTED_TEXT_COLOR,
+        <button
+        className={[
+            "chordSelector",
+            "selectable",
+            maybeClass(selectedSymbols === symbols, "emphasized"),
+            maybeClass(horizontal, "horizontal")
+        ].join(" ")}
+
+        style={{
             height: 25,
+            minWidth: CHORDS,
             fontSize: 10,
             borderRadius: 5,
             alignItems: "center",
             justifyContent: "center",
             flexGrow: 1,
-        }} onClick={(e)=>{
-            selectSymbols(symbols);
-        }}>{name}</button>
+        }} onClick={()=>{
+            selectSymbols(symbols.split(", ").map(parseFloat));
+        }}>{rootNote}{name}</button>
     );
 }
 
-const ChordSelector = ({selectSymbols}) => {
+const ChordSelector = ({horizontal, rootNote, symbols, selectSymbols}) => {
+    const selectedSymbols = symbols.join(", ");
     return (
         <div style={{
             display: "flex",
+            flexDirection: horizontal ? "column" : "row",
             justifyContent: "space-between",
         }}>
-            <ChordSelect name="M" selectSymbols={selectSymbols} symbols={[0, 4, 7]}/>
-            <ChordSelect name="m" selectSymbols={selectSymbols} symbols={[0, 3, 7]}/>
-            <ChordSelect name="7" selectSymbols={selectSymbols} symbols={[0, 4, 7, 10]}/>
-            <ChordSelect name="maj7" selectSymbols={selectSymbols} symbols={[0, 4, 7, 11]}/>
-            <ChordSelect name="m7" selectSymbols={selectSymbols} symbols={[0, 3, 7, 10]}/>
-            <ChordSelect name="half-dim7" selectSymbols={selectSymbols} symbols={[0, 3, 6, 10]}/>
-            <ChordSelect name="dim7" selectSymbols={selectSymbols} symbols={[0, 3, 6, 9]}/>
-            <ChordSelect name="6" selectSymbols={selectSymbols} symbols={[0, 4, 7, 9]}/>
-            <ChordSelect name="aug" selectSymbols={selectSymbols} symbols={[0, 4, 8]}/>
+            <ChordSelect selectedSymbols={selectedSymbols} horizontal={horizontal} rootNote={rootNote} name="" selectSymbols={selectSymbols} symbols={"0, 4, 7"}/>
+            <ChordSelect selectedSymbols={selectedSymbols} horizontal={horizontal} rootNote={rootNote} name="m" selectSymbols={selectSymbols} symbols={"0, 3, 7"}/>
+            <ChordSelect selectedSymbols={selectedSymbols} horizontal={horizontal} rootNote={rootNote} name="7" selectSymbols={selectSymbols} symbols={"0, 4, 7, 10"}/>
+            <ChordSelect selectedSymbols={selectedSymbols} horizontal={horizontal} rootNote={rootNote} name="△" selectSymbols={selectSymbols} symbols={"0, 4, 7, 11"}/>
+            <ChordSelect selectedSymbols={selectedSymbols} horizontal={horizontal} rootNote={rootNote} name="m7" selectSymbols={selectSymbols} symbols={"0, 3, 7, 10"}/>
+            <ChordSelect selectedSymbols={selectedSymbols} horizontal={horizontal} rootNote={rootNote} name="ø7" selectSymbols={selectSymbols} symbols={"0, 3, 6, 10"}/>
+            <ChordSelect selectedSymbols={selectedSymbols} horizontal={horizontal} rootNote={rootNote} name="o7" selectSymbols={selectSymbols} symbols={"0, 3, 6, 9"}/>
+            <ChordSelect selectedSymbols={selectedSymbols} horizontal={horizontal} rootNote={rootNote} name="6" selectSymbols={selectSymbols} symbols={"0, 4, 7, 9"}/>
+            <ChordSelect selectedSymbols={selectedSymbols} horizontal={horizontal} rootNote={rootNote} name="aug" selectSymbols={selectSymbols} symbols={"0, 4, 8"}/>
         </div>
     );
 }
@@ -162,11 +189,17 @@ const Tunings = [
 ];
 
 
-const Tuning = ({selected, tuning, selectTuning}) => {
+const Tuning = ({horizontal, selected, tuning, selectTuning}) => {
     return (
-        <button style={{
-            color: selected ? SELECTED_TEXT_COLOR: UNSELECTED_TEXT_COLOR,
-            backgroundColor: selected ? SELECTED_BACKGROUND_COLOR : UNSELECTED_BACKGROUND_COLOR,
+        <button
+            className={[
+                "tuningSelector",
+                "selectable",
+                maybeClass(selected, "emphasized"),
+                maybeClass(horizontal, "horizontal")
+            ].join(" ")}
+        
+        style={{
             width: ROOTS,
             display: "flex",
             fontSize: 14,
@@ -186,7 +219,7 @@ const TuningSelector = ({tuning, selectTuning, horizontal}) => {
             justifyContent: "space-between"
         }}>
             {Tunings.map((thisTuning) => {
-                return <Tuning key={thisTuning} selected={tuning==thisTuning} tuning={thisTuning} selectTuning={selectTuning} />;
+                return <Tuning horizontal={horizontal} key={thisTuning} selected={tuning==thisTuning} tuning={thisTuning} selectTuning={selectTuning} />;
             })}
         </div>
     );
@@ -206,67 +239,66 @@ const GetZerosForTuning = (tuning) => {
     }
 }
 
-class App extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            width: undefined,
-            height: undefined,
-            rootNote: "E",
-            symbols: [0, 3, 7],
-            length: 12,
-            tuning: "standard"
-        };
+const GetDimensions = () => {
+    var iOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream; 
+    let width = 0;
+    let height = 0;
+    if (iOS) {
+        width = window.orientation === 0 ? screen.width : screen.height;
+        height = window.orientation === 0 ? screen.height : screen.width;
+    } else {
+        width = window.innerWidth;
+        height = window.innerHeight;
     }
-
-    updateDimensions = () => {
-        var iOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream; 
-        let width = 0;
-        let height = 0;
-        if (iOS) {
-            width = window.orientation === 0 ? screen.width : screen.height;
-            height = window.orientation === 0 ? screen.height : screen.width;
-        } else {
-            width = window.innerWidth;
-            height = window.innerHeight;
-        }
-        setTimeout(()=>{this.setState({width: width, height: height});}, 0);
+    return {
+        width, height
     };
+}
 
-    componentWillMount() {
-        this.updateDimensions();
-    }
+const App = () => {
+    const [rootNote, setRootNote] = useState("E");
+    const [symbols, setSymbols] = useState([]);
+    const [length, setLength] = useState(12);
+    const [tuning, setTuning] = useState("standard");
+    const [dimensions, setDimensions] = useState(GetDimensions());
 
-    componentDidMount(){
-        window.addEventListener("resize", this.updateDimensions);
-        document.addEventListener("orientationchange", this.updateDimensions);
-    }
+    useEffect(()=>{
+        const updateDimensions = () => {
+            setDimensions(GetDimensions());
+        };
+        window.addEventListener("resize", updateDimensions);
+        document.addEventListener("orientationchange", updateDimensions);
+        return () => {
+            window.removeEventListener("resize", updateDimensions);
+            document.removeEventListener("orientationchange", updateDimensions);
+        }
+    }, [setDimensions]);
 
-    componentWillUnmount() {
-        window.removeEventListener("resize", this.updateDimensions);
-        document.removeEventListener("orientationchange", this.updateDimensions);
-    }
-
-    render() {
-        const horizontal = this.state.width > this.state.height;
-        const size = (Math.max(this.state.width, this.state.height) - (ROOTS + NUT + 2 * PADDING)) / (this.state.length + 1);
-        return (
-            <div style={{display: "flex", flexDirection: horizontal ? "row" : "column", height: "100%", width: "100%"}}>
-                <RootSelector horizontal={horizontal} selectedRoot={this.state.rootNote} selectRoot={(rootNote)=>{
-                    this.setState({rootNote: rootNote});
-                }}/>
-                <TuningSelector horizontal={horizontal} tuning={this.state.tuning} selectTuning={(tuning)=>{this.setState({tuning: tuning})}} />
-                <Neck zeros={GetZerosForTuning(this.state.tuning)} size={size} horizontal={horizontal} rootNote={this.state.rootNote} symbols={this.state.symbols} length={this.state.length} selectSymbol={(symbol) => {
-                    if (this.state.symbols.indexOf(symbol) >= 0) {
-                        this.state.symbols.splice(this.state.symbols.indexOf(symbol), 1);
+    const horizontal = dimensions.width > dimensions.height;
+    const majorDimension = Math.max(dimensions.width, dimensions.height);
+    const minorDimension = Math.min(dimensions.width, dimensions.height);
+    const size = Math.min(
+        (majorDimension - (ROOTS + CHORDS + TUNINGS + NUT + 2 * PADDING)) / (length + 1),
+        ((minorDimension - (6 * PADDING)) / 6)
+    );
+    return (
+        <div style={{display: "flex", flexDirection: horizontal ? "row" : "column", height: "100%", width: "100%"}}>
+            <TuningSelector horizontal={horizontal} tuning={tuning} selectTuning={(tuning)=>{setTuning(tuning);}} />
+            <RootSelector horizontal={horizontal} selectedRoot={rootNote} selectRoot={(rootNote)=>{
+                setRootNote(rootNote);
+            }}/>
+            <ChordSelector horizontal={horizontal} rootNote={rootNote} symbols={symbols} selectSymbols={setSymbols} />
+            <Neck zeros={GetZerosForTuning(tuning)} size={size} horizontal={horizontal} rootNote={rootNote} symbols={symbols} length={length} selectSymbol={(symbol) => {
+                setSymbols((symbols) => {
+                    if (symbols.indexOf(symbol) >= 0) {
+                        return update(symbols, {$splice: [[symbols.indexOf(symbol), 1]]}).sort();
                     } else {
-                        this.state.symbols.push(symbol);
+                        return update(symbols, {$push: [symbol]}).sort();
                     }
-                    this.setState({symbols: this.state.symbols});
-                }}/>
-            </div>
-        );
-    }
+                });
+            }}/>
+        </div>
+    );
 }
 
 
